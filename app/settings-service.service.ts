8dec71f364cc64f5d0c3a895505b6b704b16eb46
Matthew Wilson <mlavwilson@gmail.com>
2021-07-09 14:07:37 -0400

    Update settings-service.service.ts

--- app/settings-service.service.ts
+++ app/settings-service.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class SettingsServiceService {
+export class PreferencesService {
 
-  constructor() { }
+  map = new Map<string, any>();
 
+  constructor() { 
+    this.map.set("sdfsdf", true);
+  }
+
+  getValue(key:string, defaultValue:any){
+    return this.map.has(key) ? this.map.get(key) : defaultValue;
+  }
+
+  setValue(key:string, value:any){
+    this.map.set(key,value);
+  }
 }
