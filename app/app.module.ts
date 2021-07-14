f51d89041300ca5e61940a78ad1f6ee258af7731
followsclosely <mlavwilson@gmail.com>
2021-07-14 09:02:03 -0400

    1 file modified, 1 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -75,24 +75,3 @@ import { MatchFormComponent } from './match-form/match-form.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-
-// original
-
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { FormsModule } from '@angular/forms';
-
-// import { AppComponent } from './app.component';
-// import { HelloComponent } from './hello.component';
-
-// @NgModule({
-//   imports:      [ BrowserModule, FormsModule ],
-//   declarations: [ AppComponent, HelloComponent ],
-//   bootstrap:    [ AppComponent ]
-// })
-// export class AppModule { }
-
-
-
