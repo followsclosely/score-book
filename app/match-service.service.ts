4b3a94b2ff4489650a6dd80c68d03c7567d7f94a
followsclosely <mlavwilson@gmail.com>
2021-07-21 09:45:39 -0400

    1 file modified

--- app/match-service.service.ts
+++ app/match-service.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
-import { GameType } from './match';
+import { Match, GameType } from './match';
+
 
 @Injectable()
 export class MatchService {
+
+  private sequence:number = 1;
+  private matches = new Map<number, Match>();
+
+  //matches: Match[] = [];
 
   gameOptions = [
     new GameType("Chess", 2), 
     new GameType("Dr. Mario"), 
     new GameType("Hearts", 4),
-    new GameType("Rook", 4, true)
+    new GameType("Rook", 2, true)
   ] 
 
   constructor() { }
 
   getMyGameOptions(){
     return this.gameOptions
+  }
+
+  addMatch(match:Match){
+    this.matches.set(this.sequence++, match);
+  }
+
+  getMatch(id:number){
+    return this.matches.get(id);
   }
 
 }
