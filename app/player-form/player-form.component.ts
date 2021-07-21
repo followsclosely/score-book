160389ceed5d3db1e6e59cde2a111c64e56945aa
followsclosely <mlavwilson@gmail.com>
2021-07-21 08:43:46 -0400

    2 files modified

--- app/player-form/player-form.component.ts
+++ app/player-form/player-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../player';
+import { LogService } from '../log-service.service';
+import {MatDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-player-form',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerFormComponent implements OnInit {
 
-  constructor() { }
+  public player:Player;
+
+  constructor(
+    private dialogRef:  MatDialogRef<PlayerFormComponent>,
+    private logger:LogService
+  ) { }
 
   ngOnInit() {
+    this.player = new Player();
+    this.logger.log('PlayerFormComponent:ngOnInit');
   }
 
+  onSubmit() {
+    //alert('Thanks for submitting! Data: ' + JSON.stringify(this.game));
+    this.logger.log(this.player);
+    this.dialogRef.close();
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
