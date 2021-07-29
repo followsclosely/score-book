21a61878dd6a82482ce7f832a6d531617788481e
followsclosely <mlavwilson@gmail.com>
2021-07-29 15:51:07 -0400

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,38 +8,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
 import {
-  MatAutocompleteModule,
   MatButtonModule,
   MatButtonToggleModule,
   MatCardModule,
   MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
   MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
   MatIconModule,
   MatInputModule,
-  MatListModule,
   MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
   MatSelectModule,
   MatSidenavModule,
-  MatSliderModule,
   MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
   MatStepperModule,
   MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
