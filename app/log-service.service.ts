f7c646807d0868cee1368e008089e44f72142fa6
Matthew L. Wilson <mlavwilson@gmail.com>
2021-07-30 07:53:06 -0400

    Removed old app

--- app/log-service.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import {formatDate} from '@angular/common';
-
-@Injectable()
-export class LogService {
-    log(msg:string, bean: any) {
-        console.log(formatDate(Date.now(), 'yyyy-MM-dd hh:mm', 'en-US') + ": " + msg + " - " + JSON.stringify(bean));
-    }
-}
--- app/player.ts
+++
@@ -1,7 +0,0 @@
-export class Player {
-  constructor(
-    public id?: number,
-    public name?: string,
-    public email?: string,
-  ) {}
-}
